0a6fb7e835abf6fba9e2e29dab8cbc2226e55e53
ketaki rathore <ketakirrathore@gmail.com>
2025-08-21 10:55:17 +0530

    first commit

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+Data types
+1. Dynamically typed language : python     *statistically typed language
+ex. a=10 no need to mention that data typeswhere you mention the value
+int string float boolean complex   data types of python
+set list tuple dictionary
+
+
+integer  ex. a = 10, the value of a 
+# print(type(a)) to check the type ofdata 'int'
+
+string = "1" , '2'
+single quote or double quote value called string datatype 'str'
+
+float - decimal type ex 12.3
+'float'
+
+boolean = true and false "bool"
+
+complex = a+bj  j= #print(a.img) #printt(a.real)
+value stored in this system called complex.
+
+set = collection of numbers, store multiple value in single variable
+ex. s = {12}
+it always store in curly bracket, always need to have a value, it doesnt count repeted numbers, it change the sequence of numbers
+
+list =a = [any type of value can be store ex 1,2,4 23.4, "python"]
+to check the length of value stoed in bracket # print(len(l))= 3: count of values
+
+tuple = t=(12,)
+# print(type(t)) = 'tuple'  you have to add "," always to get tuple value
+
+dictionary 
+d={ "key":"value", "name": "john"}
+type(d)= 'dict'
+the format of value store in dictionary is in "key": "value" it is collection of pairs
+
+
+
+
